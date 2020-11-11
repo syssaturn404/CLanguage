f5798e0644445b6c3d6354b669201c7c029a8db0
Syssaturn404 <73187606+syssaturn404@users.noreply.github.com>
2020-11-12 04:37:50 +0700

    Create Misc_Operator.c

---
+++ Misc_Operator.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+void main(){
+    
+    // membuat pointer
+    int *a = 5;
+
+    // ambil alamat memori dari pointer a
+    printf("alamat memori variabel a adalah = %x\n", &a);
+}
