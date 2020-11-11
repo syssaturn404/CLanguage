ccc5d6f009e7c5c6c3e501b0e19886afa3969887
Syssaturn404 <73187606+syssaturn404@users.noreply.github.com>
2020-11-12 01:08:01 +0700

    Create Arithmatic.c

---
+++ Arithmatic.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+void main(){
+
+    // var a, b, result = a + b 
+    int a, b, result;
+    
+    // input user 
+    printf("Insert Value A : ");
+    scanf("%i", &a);
+
+    printf("Insert Value B : ");
+    scanf("%i", &b);
+
+    // increment
+    result = a + b;
+
+    printf("Result a + b : %i\n", result);
+
+}
