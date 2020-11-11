90b7315e273c83ddc64d51ff281ec0d45cf8565c
Syssaturn404 <73187606+syssaturn404@users.noreply.github.com>
2020-11-12 04:25:01 +0700

    Create Comparison_Operator.c

---
+++ Comparison_Operator.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+void main(){
+    
+    /**type data int dengan variable a diisi dengan nilai 4
+    variable b diisi dengan nilai 3**/
+    int a = 4;
+    int b = 3;
+   
+    // keterangan
+    printf("a = %d\n", a);
+    printf("b = %d\n", b);
+    
+    // operator pembanding
+    printf("a > b = %d\n", a > b);
+    printf("a < b = %d\n", a < b);
+    printf("a >= b = %d\n", a >= b);
+    printf("a <= b = %d\n", a <= b);
+    printf("a == b = %d\n", a == b);
+    printf("a != b = %d\n", a != b);
+}
