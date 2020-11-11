969a0a8b25dd39414b7fa34f20cb5d36c9d5619f
Syssaturn404 <73187606+syssaturn404@users.noreply.github.com>
2020-11-11 18:22:47 +0700

    Create helloworld.c

---
+++ helloworld.c
@@ -0,0 +1,7 @@
+#include <stdio.h> 
+    int main (){
+        printf ("hello_world \n");
+        printf("This is text %s\n","output");
+        printf("value or value %d\n",20);
+        return 0;
+    }
