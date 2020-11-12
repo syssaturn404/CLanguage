242dd71e7579aed479a6c1b675e55bfaac9ac4b0
Syssaturn404 <73187606+syssaturn404@users.noreply.github.com>
2020-11-12 20:52:40 +0700

    Create Array_Multidimensi.c

---
+++ Array/Array_Multidimensi.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+    int matriks[3][3] = {
+        {1, 3, 5},
+        {5, 3, 1},
+        {6, 2, 3}
+    };
+
+    printf("Isi Data pada indeks ke-(1,0): %d\n", matriks[1][0]);
+    
+return 0;
+}
