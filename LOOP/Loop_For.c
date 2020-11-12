d65dc3ca251e4a78f46a46d967a1228fe5ff5302
Syssaturn404 <73187606+syssaturn404@users.noreply.github.com>
2020-11-12 19:55:11 +0700

    Create Loop_For.c

---
+++ LOOP/Loop_For.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+void main(){
+    
+     // counter = 0, counter < 50 +2 
+     for(int counter = 0; counter < 50; counter+=2){
+        printf("Perulangan ke-%i\n", counter);
+    }
+}
