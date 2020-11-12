63460c3deb193ebb2da4dc5eb75484ff2dd3e688
Syssaturn404 <73187606+syssaturn404@users.noreply.github.com>
2020-11-13 05:49:04 +0700

    Create FungsiVOID.c

---
+++ FungsiVOID.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+// membuat fungsi say_hello()
+void say_hello(){
+    printf("Hello Selamat Datang!\n");
+}
+
+void main(){
+  
+    // memanggil fungsi say_hello()
+    say_hello();
+    say_hello();
+    say_hello();
+    say_hello();
+    say_hello();
+}
