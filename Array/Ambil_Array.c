8ed3a50c4f3487d43ef7dd058f5e97adf713a8e5
Syssaturn404 <73187606+syssaturn404@users.noreply.github.com>
2020-11-13 05:39:45 +0700

    Create Ambil_Array.c

---
+++ Array/Ambil_Array.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+   
+   void main(){
+    
+     //mengisi variable nama dengan char atau bernilai 1 
+     char abjad[5] = {'a' , 'b' , 'c' , 'd' , 'e'};
+     
+     /** kita panggil misal huruf e
+     %c berguna untuk menampilkan karakter **/ 
+     printf("Huruf : %c\n", abjad[4]);
+}
