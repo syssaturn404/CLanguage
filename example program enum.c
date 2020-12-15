1f66ff18c808a3315339987a885d0a1ff172aeda
Syssaturn404 <73187606+syssaturn404@users.noreply.github.com>
2020-12-15 17:36:19 +0700

    Create example program enum.c

---
+++ example program enum.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+enum boolean {false, true};
+
+void main() {
+    int a = 4;
+    int b = 5;
+
+    // kita memakai percabangan satu blok 
+    if(a < b == true){
+        printf("variabel a lebih kecil dari b\n");
+    }
+}
