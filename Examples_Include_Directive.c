88243691d83ff51fb2280c5d24a8eb4bcd719c19
Syssaturn404 <73187606+syssaturn404@users.noreply.github.com>
2020-12-15 19:48:27 +0700

    Create Examples_Include_Directive.c

---
+++ Examples_Include_Directive.c
@@ -0,0 +1,7 @@
+#include <stdio.h> / * include file header standar. * /
+#include "D:myfile.c" / * Sertakan file yang ditentukan pengguna. * /
+int main (void)
+{
+     printf ("Perhatikan penggunaan #Include pre-processor");
+     return 0;
+}
